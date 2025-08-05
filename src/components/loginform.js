23863dbc05347eed6c4514d123018ff02b622fe9
DIEGO MOURA RODRIGUES <diego.mrodrigues13@senacsp.edu.br>
2025-08-05 17:29:55 -0300

    atualização

--- src/components/loginform.js
+++ src/components/loginform.js
@@ -5,18 +5,20 @@ export default function (){
     const email = document.createElement('input');
     email.type = 'email';
     email.placeholder = "digite seu e-mail";
-    email.className = "imput-email";
+    email.classname = "imputs";
     formulario.appendChild(email);
 
     //senha
     const senha = document.createElement('input');
     senha.type = 'senha';
     senha.placeholder = "digite sua senha ";
+    senha.className = "imputs-senha";
     formulario.appendChild(senha);
 
     //button de entar
     const entar = document.createElement('button');
     entar.type = 'submit';
+    entar.classname = "button-login"
     entar.textContent = "Entrar";
     formulario.appendChild(entar);
 
--- src/pages/login.js
+++ src/pages/login.js
@@ -4,8 +4,15 @@ export default function renderLoginPage(){
     const divRoot = document.getElementById('root');
     divRoot.innerHTML = '';
 
+    //dentro de divRoot tera uma div chamada conteiner e em conteiner estara o formulario
+    const container = document.createElement('div');
+    container.className = 'card-p-4 shadow-lg';//classe di bootstrap para criar um card
+    container.style.width = '100%'; //aplicada a largura de 100% na div container para ocupar na tela
+    container.style.maxWidth = '400px'; //ate que atinja o maximo de 400px
+    divRoot.appendChild(container); //divRoot contem a nova div
+
     const formulario = loginform();
-    divRoot.appendChild(formulario);
+    container.appendChild(formulario); // nova div container, ja dentro de divroot, contem o form
 
 }
 
