d5f9cccba922e61f35eb047a7208695a78225b69
DIEGO MOURA RODRIGUES <diego.mrodrigues13@senacsp.edu.br>
2025-08-08 17:27:00 -0300

    atualização

--- src/components/loginform.js
+++ src/components/loginform.js
@@ -1,25 +1,27 @@
 export default function (){
     const formulario = document.createElement('form');
+    formulario.className = 'd-flex flex-colunm';
 
     //email
     const email = document.createElement('input');
     email.type = 'email';
     email.placeholder = "digite seu e-mail";
-    email.classname = "imputs";
+    email.classname = 'imputs';
     formulario.appendChild(email);
 
     //senha
     const senha = document.createElement('input');
     senha.type = 'senha';
     senha.placeholder = "digite sua senha ";
-    senha.className = "imputs-senha";
+    senha.className = 'imputs-senha';
     formulario.appendChild(senha);
 
     //button de entar
     const entar = document.createElement('button');
     entar.type = 'submit';
-    entar.classname = "button-login"
+    entar.classname = 'button-login';
     entar.textContent = "Entrar";
+    entar.className = 'btn btn-primary';
     formulario.appendChild(entar);
 
     return formulario;
--- src/pages/login.js
+++ src/pages/login.js
@@ -4,6 +4,10 @@ export default function renderLoginPage(){
     const divRoot = document.getElementById('root');
     divRoot.innerHTML = '';
 
+    const titulos = document.createElement('h1');
+    titulos.textContent = 'faça o login ou crie uma conta';
+    titulos.className = 'titulos';
+
     //dentro de divRoot tera uma div chamada conteiner e em conteiner estara o formulario
     const container = document.createElement('div');
     container.className = 'card-p-4 shadow-lg';//classe di bootstrap para criar um card
@@ -11,7 +15,9 @@ export default function renderLoginPage(){
     container.style.maxWidth = '400px'; //ate que atinja o maximo de 400px
     divRoot.appendChild(container); //divRoot contem a nova div
 
+    
     const formulario = loginform();
+    container.appendChild(titulos);
     container.appendChild(formulario); // nova div container, ja dentro de divroot, contem o form
 
 }
