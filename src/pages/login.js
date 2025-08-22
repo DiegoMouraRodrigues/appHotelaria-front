3e43e3b2db249da6fdb8f8fee925a51f389ae94a
DIEGO MOURA RODRIGUES <diego.mrodrigues13@senacsp.edu.br>
2025-08-22 17:09:38 -0300

    atualização

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,38 +1,13 @@
-import loginform from "../components/loginform.js";
-import NavBar from "../components/Menu.js";
+import Form from "../components/Form.js";
+import Navbar from "../components/Navbar.js";
 
-export default function renderLoginPage(){
-
+export default function renderLoginPage() { 
     const nav = document.getElementById('navbar');
     nav.innerHTML = '';
-
-    const navbar = NavBar();
+    
+    const navbar = Navbar();
     nav.appendChild(navbar);
 
-    const divRoot = document.getElementById('root');
-    divRoot.innerHTML = '';
-
-    const titulos = document.createElement('h1');
-    titulos.textContent = 'faça o login ou crie uma conta';
-    titulos.className = 'titulos';
-    //centraliza somente o titulo da pagina de login
-    titulos.style.textAlign = 'center';
-
-    //dentro de divRoot tera uma div chamada conteiner e em conteiner estara o formulario
-    const container = document.createElement('div');
-    container.className = 'card-p-4 shadow-lg';//classe di bootstrap para criar um card
-    container.style.width = '100%'; //aplicada a largura de 100% na div container para ocupar na tela
-    container.style.maxWidth = '400px'; //ate que atinja o maximo de 400px
-
-    divRoot.appendChild(container); //divRoot contem a nova div
-
-    
-    const formulario = loginform();
-
-
-    container.appendChild(titulos);
-    container.appendChild(formulario); // nova div container, ja dentro de divroot, contem o form
+    Form();
 
 }
-
-
